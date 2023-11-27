59f3832deb48cc8cea89c03b28049aa46b53dde2
Patryk Szczepanik <skowik9@gmail.com>
2023-11-27 01:10:48 +0100

    Update Prime_number_looking_v3.py

--- Prime_number_looking_v3.py
+++ Prime_number_looking_v3.py
@@ -10,10 +10,10 @@ def sumowanie(liczba):
         miejsce = miejsce + 1
     return suma
 
-def prime_numver(liczba):
+def prime_numver(dolna, gorna):
     wynik = 0
-    date1 = datetime.now()
-    for sprawdzana in range(1, liczba):
+    liczba_pierwszych = 0
+    for sprawdzana in range(dolna, gorna):
         ost_liczba = int(str(sprawdzana)[-1])
         if ost_liczba == 0 or ost_liczba == 2 or ost_liczba == 4 or ost_liczba == 6 or ost_liczba == 8 or sumowanie(sprawdzana) % 3 == 0:
             wynik = wynik + 1
@@ -25,8 +25,25 @@ def prime_numver(liczba):
                     break
             if wynik == 0:
                 print((" "*19)+str(sprawdzana))
-
+                liczba_pierwszych = liczba_pierwszych + 1
         wynik = 0
+    print("Znaleziono liczb pierwszych w zakresie", dolna, "-", gorna, ":", liczba_pierwszych)
+
+
+def main():
+    print("Podaj zakres liczb do sprawdzenia \n a > 1 \n b > 1\n")
+    liczba_1 = int(input("Podaj 1 liczbe \n"))
+    liczba_2 = int(input("Podaj 2 liczbe \n"))
+
+    date1 = datetime.now()
+    if liczba_1 > 1 and liczba_2 > 1:
+        if liczba_1 > liczba_2:
+            prime_numver(liczba_2, liczba_1)
+        else:
+            prime_numver(liczba_1, liczba_2)
+    else:
+        print("Za maly zakres")
+        main()
 
     date2 = datetime.now()
     czas_minuty = int((date2 - date1).total_seconds()/60)
@@ -36,7 +53,6 @@ def prime_numver(liczba):
     print(tabulate(timer, headers='firstrow', tablefmt='fancy_grid'))
 
 try:
-    liczba = 15677
-    prime_numver(liczba)
+    main()
 except KeyboardInterrupt:
     print("XD")
